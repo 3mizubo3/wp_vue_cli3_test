41cc0090f463dbeacabe3fec1b45adf4397d61d6
mizuho <mizuho@birdman.ne.jp>
2018-11-29 19:17:50 +0900

    anyschool.vueファイルをcomponentに追加

---
+++ src/components/anyshool.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="all_wrap">
+    <div class="contents_wrap">
+        <header class="page-header">
+          <h1><a href=""><img src="" alt="">Any School</a></h1>
+          <div class="menu">
+            <p class="menu_item"><a href="">Any Schoolとは</a></p>
+            <p class="menu_item"><a href="">活動報告</a></p>
+            <p class="menu_item info"><a href="">お問い合わせ</a></p>
+          </div>
+        </header>
+        <main>
+        <section class="top">
+          <h2 class="top_title">きみは、ひとりしかいない。<br>きみは、ひとりじゃない。</h2>
+          <p class="top_text">
+            記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト<br>
+            記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト<br>
+            記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト<br>
+            記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト記事テキスト<br>
+          </p>
+        </section>
+        <section class="news">
+          <h3 class="common_title">News</h3>
+          <p class="common_text">エニースクールの最新情報</p>
+          <table class="news_table contents_item" border="1" rules="rows">
+            <tr class="news_list">
+              <th>2018.11.12</th>
+              <th>記事テキスト記事テキスト記事テキスト</th>
+            </tr>
+            <tr class="news_list">
+              <th>2018.11.12</th>
+              <th>記事テキスト記事テキスト記事テキスト</th>
+            </tr>
+          </table>
+        </section>
+
+        <section class="report">
+          <h3 class="common_title">Report</h3>
+          <p class="common_text">エニースクールの活動報告</p>
+          <div class="contents_item movie">
+            <div class="movie_item"><a href=""><img src="../assets/img/info1.jpg" alt=""></a></div>
+            <div class="movie_item"><a href=""><img src="../assets/img/info2.jpg" alt=""></a></div>
+          </div>
+          <ul class="contents_item report_group">
+            <li class="report_list"><a href=""><img src="" alt=""><p>◯◯してきました</p></a></li>
+            <li class="report_list"><a href=""><img src="" alt=""><p>◯◯してきました</p></a></li>
+            <li class="report_list"><a href=""><img src="" alt=""><p>◯◯してきました</p></a></li>
+            <li class="report_list"><a href=""><img src="" alt=""><p>◯◯してきました</p></a></li>
+            <li class="report_list"><a href=""><img src="" alt=""><p>◯◯してきました</p></a></li>
+            <li class="report_list"><a href=""><img src="" alt=""><p>◯◯してきました</p></a></li>
+          </ul>
+          <p class="seemore">もっとみる</p>
+        </section>
+        </main>
+
+        <section class="sponsor">
+          <h3 class="common_title">Sponsor</h3>
+          <p class="common_text">エニースクールの活動報告</p>
+          <ul class="contents_item sponsor_group">
+            <li class="sponsor_list"><a href=""><img src="" alt="">dentsu</a></li>
+            <li class="sponsor_list"><a href=""><img src="" alt="">dentsu</a></li>
+            <li class="sponsor_list"><a href=""><img src="" alt="">dentsu</a></li>
+          </ul>
+        </section>
+
+        <footer class="page-footer">
+          <p class="copyright"><small>@2018 Any School</small></p>
+          <ul class="footer_group">
+            <li class="footer_item sns"><a href="">twitter</a></li>
+            <li class="footer_item sns"><a href="">facebook</a></li>
+            <li class="footer_item pagetop"><a href="">pagetop</a></li>
+          </ul>
+        </footer>
+      </div>
+  </div>
+</template>
+
+
+
+<style scoped src="../assets/css/app.scss" lang="scss">
+
+</style>
